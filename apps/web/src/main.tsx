cf0ed739388fc056ba23e7e656b3d996d9f839cc
Simon Wang <simonwang384@gmail.com>
2026-04-24 14:10:25 -0400

    feat: tanstack router and query

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App.tsx'
+import { routeTree } from './routeTree.gen'
+import { theme } from './theme'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-)
+const queryClient = new QueryClient()
+
+const router = createRouter({
+	routeTree,
+	scrollRestoration: true,
+	context: {
+		queryClient,
+	},
+})
+
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
+
+const rootElement = document.getElementById('root') as HTMLElement
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement)
+	root.render(
+		<StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<MantineProvider theme={theme}>
+					<RouterProvider router={router} />
+				</MantineProvider>
+			</QueryClientProvider>
+		</StrictMode>,
+	)
+}
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,13 @@
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		tanstackRouter({
+			target: 'react',
+			autoCodeSplitting: true,
+		}),
+		react(),
+	],
 })
